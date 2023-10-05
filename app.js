fefda42764a5b9a98fbfe6df056a772d697010e6
rupesh4658 <rkyadav4650@gmail.com>
2023-10-06 00:41:49 +0545

    learn about Nodemon and ejs

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express=require("express")//Requiring express packages.
 const app=express()//Storing it in app, app vanne variable throughout use garxum
+//ma ejs use garna aate ko as a view engine
+app.set("view engine","ejs")
 
 //'/' halda home page dekhaune
 app.get('/',(req,res) =>{
-    res.send("<h1>Home page</h1>")
+    //views vitra ko file render garxa
+    res.render("home")
 })
 
 // '/about' halda about page dekhaune
 app.get('/about',(req,res) =>{
-    res.send("About page")
+    res.render("about")
 })
 
 app.get('/contact',(req,res) =>{
@@ -21,6 +24,6 @@ app.get('/contact',(req,res) =>{
 
 
 //port number ->room number for certain time
-app.listen(3001,function(){
-    console.log("nodejs project has started on port 3001 ")
+app.listen(3000,function(){
+    console.log("nodejs project has started on port 3000 ")
 })
